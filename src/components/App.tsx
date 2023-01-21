3f4f6e91408e521348c67c9806395b25b7d5a2f4
Apodlubny <apodlubny@gmail.com>
2023-01-21 20:44:32 +0200

    base structure

---
+++ src/components/App.tsx
@@ -0,0 +1,7 @@
+import { FC } from "react";
+
+const App: FC = () => {
+  return <div>Hello Codebridge!</div>;
+};
+
+export default App;
---
+++ src/components/ArticleCard.tsx
@@ -0,0 +1,8 @@
+import { FC } from "react";
+import { Article } from "../helpers/types";
+
+const ArticleCard: FC<Article> = () => {
+  return <div>Article card</div>;
+};
+
+export default ArticleCard;
