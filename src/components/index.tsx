d42eeed7a2c27b85448b446c82c660d9c24b170b
Apodlubny <apodlubny@gmail.com>
2023-01-21 21:09:46 +0200

    create redux files

---
+++ src/components/index.tsx
@@ -0,0 +1,3 @@
+export { default as Filter } from "./Filter";
+export { default as ArticlesList } from "./ArticlesList";
+export { default as ArticleCard } from "./ArticleCard";
---
+++ src/redux/articlesApi.ts
@@ -0,0 +1,18 @@
+import axios from "axios";
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import { Article } from "../helpers/types";
+
+axios.defaults.baseURL = "https://api.spaceflightnewsapi.net/v3/articles";
+
+export const getLatestArticles = createAsyncThunk<
+  Article[],
+  undefined,
+  { rejectValue: string }
+>("getLatestArticles", async (_, { rejectWithValue }) => {
+  try {
+    const { data } = await axios.get("?_limit=40");
+    return data;
+  } catch (error: any) {
+    return rejectWithValue(error.message);
+  }
+});
