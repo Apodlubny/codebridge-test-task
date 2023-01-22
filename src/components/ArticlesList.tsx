538b04d72d2a52d49b38decce7b1dc32b9c16339
Apodlubny <apodlubny@gmail.com>
2023-01-22 20:48:01 +0200

    filter+articlesList component

--- src/components/ArticlesList.tsx
+++ src/components/ArticlesList.tsx
@@ -1,7 +1,29 @@
 import { FC } from "react";
+import { useAppSelector } from "../hooks/storeHooks";
+import { getFilteredList } from "../helpers/getFilteredList";
+import ArticleCard from "./ArticleCard";
+
+import { Typography, Grid, Divider } from "@mui/material";
 
 const ArticlesList: FC = () => {
-  return <div>Articles list</div>;
+  const articles = useAppSelector((state) => state.list);
+  const filter = useAppSelector((state) => state.filter);
+  const list = getFilteredList(articles, filter.split(" "));
+  return (
+    <>
+      <Typography sx={{ mt: 5, lineHeight: 1.25, fontWeight: 600 }}>
+        {filter ? `Results: ${list.length}` : `Last ${list.length} articles`}
+      </Typography>
+      <Divider sx={{ mb: "45px", borderColor: "#EAEAEA" }} />
+      <Grid container spacing={{ md: "45px" }} columns={{ md: 12 }}>
+        {list.map((article) => (
+          <Grid item md={4} key={article.id}>
+            <ArticleCard {...article} />
+          </Grid>
+        ))}
+      </Grid>
+    </>
+  );
 };
 
 export default ArticlesList;
---
+++ src/helpers/getFilteredList.tsx
@@ -0,0 +1,17 @@
+import { Article } from "./types";
+
+export const getFilteredList = (list: Article[], keywords: string[]) => {
+  if (keywords.length === 0) return list;
+  const resultTitle = list.filter((article) =>
+    keywords.some((word) =>
+      article.title.toLowerCase().includes(word.toLowerCase())
+    )
+  );
+  const resultSummary = list.filter((article) =>
+    keywords.some((word) =>
+      article.summary.toLowerCase().includes(word.toLowerCase())
+    )
+  );
+  const result = resultTitle.concat(resultSummary);
+  return result.filter((article, index) => result.indexOf(article) === index);
+};
